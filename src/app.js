36ee7351bf29a16626167d339609e10c621975d3
pallascodes <pallascodes@gmail.com>
2021-08-16 13:04:07 -0500

    update

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
 AOS.init();
 
 const btnBurguer = document.getElementById('btn-burguer')
+const btnClose = document.getElementById('btn-close')
 const mobileNav = document.getElementById('mobile-nav')
 
 btnBurguer.addEventListener('click', () => {
+  mobileNav.classList.toggle('active')
+})
+
+btnClose.addEventListener('click', () => {
   mobileNav.classList.toggle('active')
 })
