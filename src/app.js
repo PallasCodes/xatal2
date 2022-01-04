960f576c9997483c27bda94cfbb3e17bf44a7890
pallascodes <pallascodes@gmail.com>
2022-01-04 11:12:03 -0600

    navbar fixed

--- src/app.js
+++ src/app.js
@@ -1,13 +1,35 @@
 AOS.init();
 
-const btnBurguer = document.getElementById('btn-burguer')
-const btnClose = document.getElementById('btn-close')
-const mobileNav = document.getElementById('mobile-nav')
+// MOBILE NAVBAR
+const btnBurguer = document.getElementById("btn-burguer");
+const mobileNav = document.getElementById("mobile-nav");
 
-btnBurguer.addEventListener('click', () => {
-  mobileNav.classList.toggle('active')
-})
+btnBurguer.addEventListener("click", () => {
+  if (mobileNav.classList.contains("active")) {
+    mobileNav.classList.add("unActive");
+    setTimeout(() => mobileNav.classList.remove("active"), 590);
+  } else {
+    mobileNav.classList.add("active");
+    mobileNav.classList.remove("unActive");
+  }
+});
 
-btnClose.addEventListener('click', () => {
-  mobileNav.classList.toggle('active')
-})
+// NAVBAR
+const navbar = document.getElementById("nav-wrapper");
+let slidedNavbar = false;
+
+function toggleNavbar() {
+  if (window.scrollY > 150 && !slidedNavbar) {
+    navbar.classList.add("active");
+    slidedNavbar = true;
+  } else if (window.scrollY < 150 && slidedNavbar) {
+    navbar.classList.remove("active");
+    slidedNavbar = false;
+  }
+}
+
+window.addEventListener("scroll", () => {
+  toggleNavbar();
+});
+
+toggleNavbar();
